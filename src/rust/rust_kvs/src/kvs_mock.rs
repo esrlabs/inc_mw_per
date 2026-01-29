468d1a326d3b4375fe58306af991fd9b986bdd73
Dan Calavrezo <195309321+dcalavrezo-qorix@users.noreply.github.com>
2026-01-29 13:29:32 +0200

    fix: double headers
    Removed double headers
    
    Signed-off-by: Dan Calavrezo <195309321+dcalavrezo-qorix@users.noreply.github.com>

--- src/rust/rust_kvs/src/kvs_mock.rs
+++ src/rust/rust_kvs/src/kvs_mock.rs
@@ -10,17 +10,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 // *******************************************************************************
-// Copyright (c) 2025 Contributors to the Eclipse Foundation
-//
-// See the NOTICE file(s) distributed with this work for additional
-// information regarding copyright ownership.
-//
-// This program and the accompanying materials are made available under the
-// terms of the Apache License Version 2.0 which is available at
-// <https://www.apache.org/licenses/LICENSE-2.0>
-//
-// SPDX-License-Identifier: Apache-2.0
-
 use crate::error_code::ErrorCode;
 use crate::kvs_api::{KvsApi, SnapshotId};
 use crate::kvs_value::{KvsMap, KvsValue};
